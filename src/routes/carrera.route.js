cf31d7d80bd97c3cdf20c3ce470959f631009619
Ignacio Rzeszut <hectorignacio.rzeszut@estudiantes.unahur.edu.ar>
2024-05-30 15:27:45 -0300

    agregados schemas para Carrera y Profesor

--- src/routes/carrera.route.js
+++ src/routes/carrera.route.js
@@ -2,6 +2,8 @@ const {Router} = require('express')
 const {carrera} = require('../db/models')
 const carreraController = require('../controllers/carrera.controller')
 const middlewareCarrera = require('../middlewares/middleware')
+const carreraSchema = require("../schemas/carrera.schema")
+const materiaSchema = require("../schemas/materia.schema")
 const routerCarrera = Router()
 
 routerCarrera.get("/carreras", carreraController.getAllCarreras)
@@ -12,7 +14,7 @@ routerCarrera.get("/carreras/:id/materias",middlewareCarrera.existsById(carrera)
 
 routerCarrera.post("/carreras", carreraController.createCarrera)
 
-routerCarrera.post("/carreras/:id/materia", middlewareCarrera.existsById(carrera), carreraController.createMateriaInCarrera)
+routerCarrera.post("/carreras/:id/materia", middlewareCarrera.existsById(carrera),middlewareCarrera.validateSchema(materiaSchema) ,carreraController.createMateriaInCarrera)
 
 
 module.exports = routerCarrera
--- src/routes/profesor.route.js
+++ src/routes/profesor.route.js
@@ -2,23 +2,26 @@
 const {Router} = require('express')
 const {Profesor} = require('../db/models')
 const {Curso} = require('../db/models')
+const profesorSchema = require("../schemas/profesor.schema")
 const profesorController = require('../controllers/profesor.controller')
-const middlewareProfesor = require('../middlewares/middleware')
+const profesorMiddleware = require('../middlewares/middleware')
 const routeProfesor = Router()
 
 routeProfesor.get("/profesores", profesorController.getAllProfesores)
 
-routeProfesor.get("/profesores/:id", middlewareProfesor.existsById(Profesor), profesorController.getProfesorById)
+routeProfesor.get("/profesores/:id", profesorMiddleware.existsById(Profesor), profesorController.getProfesorById)
 
-routeProfesor.get("/cursos/:id/profesores", middlewareProfesor.existsById(Curso) ,profesorController.getProfesoresCurso)
+routeProfesor.get("/cursos/:id/profesores", profesorMiddleware.existsById(Curso) ,profesorController.getProfesoresCurso)
 
 routeProfesor.post("/profesores",profesorController.createProfesor)
 
-routeProfesor.put("/profesores/:id", profesorController.modifyProfesorById)
+routeProfesor.put("/profesores/:id", profesorMiddleware.validateSchema(profesorSchema),profesorController.modifyProfesorById)
 
-routeProfesor.delete("/profesores/:id",middlewareProfesor.existsById(Profesor) ,profesorController.deleteProfesorById)
+routeProfesor.delete("/profesores/:id",profesorMiddleware.existsById(Profesor) ,profesorController.deleteProfesorById)
 
 //route.post("/cursos/:id/profesores", profesorController.asociarProfeCurso) ni idea como se hace este jaja
 
-routeProfesor.get("/profesores/:id/cursos", middlewareProfesor.existsById(Profesor) ,profesorController.getCursosProfesor)
+routeProfesor.get("/profesores/:id/cursos", profesorMiddleware.existsById(Profesor) ,profesorController.getCursosProfesor)
+
+
 module.exports = routeProfesor
